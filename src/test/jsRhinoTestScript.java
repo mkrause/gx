639b8201399b5eebeda215c02e1b894dd0fc3a3e
Erik van der Veen <erikvdv1@gmail.com>
2013-04-19 10:50:40 +0200

    Added Rhino lib and test script

--- src/test/jsRhinoTestScript.java
+++ src/test/jsRhinoTestScript.java
@@ -1,34 +1,48 @@
 package test;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.BufferedReader;
 
-import javax.script.*;
+import org.mozilla.javascript.*;
 
 public class jsRhinoTestScript {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		Context context = Context.enter();
-		try {
-			BufferedReader script = new BufferedReader(new FileReader(new File("src/test/testScript.js")));
-		    ScriptableObject scope = context.initStandardObjects();
-		    context.evaluateString(scope, script, "script", 1, null);
-		    
-		    Object helloVar = scope.get("helloVar", scope);
-		    System.out.println("returned helloVar: " + helloVar);
-		    
-		    Function fct = (Function)scope.get("helloLazyFunction", scope);
-		    Object result = fct.call(
-		            context, scope, scope, new Object[] {"one", "two"});
-		    System.out.println("Returned from function: " + Context.jsToJava(result, int.class));
-		} finally {
-		    Context.exit();
-		}
-	}
+        // Creates and enters a Context. The Context stores information
+         // about the execution environment of a script.
+         Context cx = Context.enter();
+         try {
+             // Initialize the standard objects (Object, Function, etc.)
+             // This must be done before scripts can be executed. Returns
+             // a scope object that we use in later calls.
+             Scriptable scope = cx.initStandardObjects();
+             
+             Object jsOut = Context.javaToJS(System.out, scope);
+             ScriptableObject.putProperty(scope, "stdout", jsOut);
+             
+             BufferedReader br = new BufferedReader(new FileReader(new File("src/test/testScript.js")));
+             cx.evaluateReader(scope, br, "<cmd>", 1, null);
+
+             System.out.println(scope.get("helloVar", scope));
+             //XXX: Note that the helloFunction is executed in the js script by default. This get call does not invoke the function
+             System.out.println(scope.get("helloFunction", scope));
+             
+             Function fct = (Function)scope.get("helloLazyFunction", scope);
+             Object result = fct.call(
+                     cx, scope, scope, new Object[] {"hello", 3});
+             System.out.println(Context.jsToJava(result, String.class));
+ 
+         } catch (Exception e) {
+             e.printStackTrace();
+         } finally {
+             // Exit from the context.
+             Context.exit();
+         }
+    }
 
 }
