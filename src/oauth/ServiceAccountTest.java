f1b56fc641dda63680ca8999750fba07d585ee49
Erik van der Veen <erikvdv1@gmail.com>
2013-04-23 13:29:42 +0200

    Google java oauth

---
+++ src/oauth/ServiceAccountTest.java
@@ -0,0 +1,123 @@
+package oauth;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.tasks.*;
+import com.google.api.services.tasks.model.*;
+import com.google.api.services.calendar.*;
+import com.google.api.services.calendar.model.*;
+import com.google.api.services.drive.*;
+import com.google.api.services.drive.model.*;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+/**
+ * @author Yaniv Inbar
+ */
+public class ServiceAccountTest {
+
+  /** E-mail address of the service account. */
+    //private static final String SERVICE_ACCOUNT_EMAIL = "289978787145-qoafq30a0i1qou5eul1hvepc99jnglua@developer.gserviceaccount.com";
+    private static final String SERVICE_ACCOUNT_EMAIL = "289978787145-qoafq30a0i1qou5eul1hvepc99jnglua@developer.gserviceaccount.com";
+
+  /** Global configuration of Google Cloud Storage OAuth 2.0 scope. */
+  private static final String SCOPES[] = new String[] {
+      com.google.api.services.tasks.TasksScopes.TASKS,
+      com.google.api.services.calendar.CalendarScopes.CALENDAR,
+      com.google.api.services.drive.DriveScopes.DRIVE
+  };
+
+  /** Global instance of the HTTP transport. */
+  private static HttpTransport HTTP_TRANSPORT;
+
+  /** Global instance of the JSON factory. */
+  private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+  public static void main(String[] args) {
+    try {
+      try {
+        HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+
+        // Build service account credential.
+        GoogleCredential credential = new GoogleCredential.Builder()
+            .setTransport(HTTP_TRANSPORT)
+            .setJsonFactory(JSON_FACTORY)
+            .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
+            .setServiceAccountScopes(SCOPES)
+            .setServiceAccountPrivateKeyFromP12File(new java.io.File("key.p12"))
+            .build();
+
+        credential.refreshToken();
+        System.out.println(credential.getAccessToken());
+
+//        com.google.api.services.tasks.Tasks service =
+//            new com.google.api.services.tasks.Tasks.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+//                .setApplicationName("Realtime-Gx/1.0").build();
+//
+//        // Get tasks
+//        TaskLists taskLists = service.tasklists().list().execute();
+//
+//        for (TaskList taskList : taskLists.getItems()) {
+//            System.out.println("List: " + taskList.getTitle());
+//          }
+
+//        String taskListId = (taskLists.getItems()).get(0).getId();
+//        com.google.api.services.tasks.model.Tasks tasks = service.tasks().list(taskListId).execute();
+//        com.google.api.services.tasks.model.Tasks tasks = service.tasks().list("@default").execute();
+//        if (tasks.getItems() != null) {
+//            for (Task task : tasks.getItems()) {
+//              System.out.println("Task: " + task.getTitle());
+//            }
+//        }
+        
+        
+        
+//        com.google.api.services.calendar.Calendar service =
+//                new com.google.api.services.calendar.Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+//                .setApplicationName("Realtime-Gx/1.0").build();
+//        
+//        CalendarList calendarList = service.calendarList().list().execute();
+//        
+//        for (CalendarListEntry calendar : calendarList.getItems()) {
+//          System.out.println("Calendar: " + calendar.getSummary());
+//        }
+        
+      com.google.api.services.drive.Drive service =
+              new com.google.api.services.drive.Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+              .setApplicationName("Realtime-Gx/1.0").build();
+      
+      // Add file
+//      File body = new File();
+//      body.setTitle("A sample file");
+//      body.setDescription("Beschrijving");
+//      body.setMimeType("application/vnd.google-apps.document");
+//      service.files().insert(body).execute();
+      
+      FileList files = service.files().list().execute();
+      
+      for (com.google.api.services.drive.model.File file : files.getItems()) {
+        System.out.println("File: " + file.getTitle());
+      }
+      
+      System.exit(0);
+
+      } catch (IOException e) {
+        System.err.println(e.getMessage());
+      }
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+    System.exit(1);
+  }
+}
