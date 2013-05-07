ddc56159aa0e15f5b721e4884ee02cd11519573c
Yorick Holkamp <github@nextpulse.nl>
2013-05-07 12:34:21 +0200

    Introduced a query builder to construct the query parameters with method chaining

---
+++ src/channel/browser/util/URLQueryBuilder.java
@@ -0,0 +1,36 @@
+package channel.browser.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Helper class that allows for chained construction of parameter map.
+ */
+public class URLQueryBuilder
+{
+    Map<String, String> parameters = new HashMap<String, String>();
+
+    public URLQueryBuilder put(String key, String value) {
+        parameters.put(key, value);
+        return this;
+    }
+
+    public URLQueryBuilder put(String key, int value) {
+        parameters.put(key, Integer.toString(value));
+        return this;
+    }
+
+    public URLQueryBuilder put(String key, long value) {
+        parameters.put(key, Long.toString(value));
+        return this;
+    }
+
+    public URLQueryBuilder put(String key, Object value) {
+        parameters.put(key, value.toString());
+        return this;
+    }
+
+    public Map<String,String> build() {
+        return parameters;
+    }
+}
