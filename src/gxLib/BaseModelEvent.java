6241bf61fcb8000949ed01d1fa09b8516bc2875e
Roy de Bokx <info@debokxict.nl>
2013-05-15 14:17:50 +0200

    adding BaseModelEvent and stub of textDeletedEvent

---
+++ src/gxLib/BaseModelEvent.java
@@ -0,0 +1,63 @@
+package gxLib;
+
+/**
+ * @author Rdebokx
+ */
+public class BaseModelEvent {
+
+    private String type;
+    private EventTarget target;
+    private String sessionId;
+    private boolean isLocal;
+    private boolean bubbles;
+
+    public BaseModelEvent(String type, EventTarget target, String sessionId, boolean isLocal, boolean bubbles){
+        this.type = type;
+        this.target = target;
+        this.sessionId = sessionId;
+        this.isLocal = isLocal;
+        this.bubbles = bubbles;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public EventTarget getTarget() {
+        return target;
+    }
+
+    public void setTarget(EventTarget target) {
+        this.target = target;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public boolean isLocal() {
+        return isLocal;
+    }
+
+    public void setLocal(boolean local) {
+        isLocal = local;
+    }
+
+    public boolean isBubbles() {
+        return bubbles;
+    }
+
+    public void setBubbles(boolean bubbles) {
+        this.bubbles = bubbles;
+    }
+
+
+}
---
+++ src/gxLib/TextDeletedEvent.java
@@ -0,0 +1,12 @@
+package gxLib;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rdebokx
+ * Date: 5/15/13
+ * Time: 13:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class TextDeletedEvent extends BaseModelEvent{
+
+}
