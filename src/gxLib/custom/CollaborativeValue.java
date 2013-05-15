b826fa6e19d37148a4fc9e08ee36f1c6c8032462
Roy de Bokx <info@debokxict.nl>
2013-05-15 16:15:05 +0200

    made stubs for CustomObject and CollaborativeValue, used within the CollaborativeCustomObject

---
+++ src/gxLib/custom/CollaborativeValue.java
@@ -0,0 +1,21 @@
+package gxLib.custom;
+
+/**
+ * @author Rdebokx
+ */
+public class CollaborativeValue {
+
+    private Object value;
+
+    public CollaborativeValue(Object value) {
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}
