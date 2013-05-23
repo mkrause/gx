adf1205eda72bb4bc1268d2898e144ba5d531988
Yorick Holkamp <github@nextpulse.nl>
2013-05-23 20:29:13 +0200

    Inserted some returns to improve the readability of the json spaghetti in our test cases.

--- test/gx/realtime/RealtimeMessageHandlerTest.java
+++ test/gx/realtime/RealtimeMessageHandlerTest.java
@@ -22,18 +22,39 @@ import static org.mockito.Mockito.mock;
 public class RealtimeMessageHandlerTest
 {
     // General events
-    final String collabJoinedMsg = "[5,1369300707776,{\"color\":\"#58B442\",\"displayName\":\"John Doe\",\"isActive\":true,\"isAnonymous\":false,\"isMe\":true,\"isNew\":true,\"photoUrl\":\"https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rsbdfgvv5M/s128/photo.jpg\",\"profileId\":\"11113136429605372\",\"sid\":\"2baa24a287e097a6\",\"userId\":\"111138071136429601111\",\"userType\":0}]";
-    final String collabLeftMsg = "[6,1369309187455,{\"sid\":\"3a7f52e3942c1fe3\",\"userId\":\"111138071136429601111\"}]";
+    final String collabJoinedMsg = "[5,1369300707776,{" +
+            "\"color\":\"#58B442\",\"displayName\":\"John Doe\",\"isActive\":true,\"isAnonymous\":false,\"isMe\":true,\"isNew\":true," +
+            "\"photoUrl\":\"https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rsbdfgvv5M/s128/photo.jpg\"," +
+            "\"profileId\":\"11113136429605372\",\"sid\":\"2baa24a287e097a6\",\"userId\":\"111138071136429601111\",\"userType\":0" +
+            "}]";
+    final String collabLeftMsg = "[6,1369309187455,{" +
+            "\"sid\":\"3a7f52e3942c1fe3\",\"userId\":\"111138071136429601111\"" +
+            "}]";
 
     // Map events
-    final String mapValueChangedMsg_delete = "[0,1369308664813,[[17,\"13ed12879c5\",\"111138071136429601111\",\"7e1c20fc1848fe70\",[4,[0,[8,\"gdegz4x7zhgc9qqir\",\"foo\"]]]]]]";
-    final String mapValueChangedMsg_add = "[0,1369308664833,[[19,\"13ed12bf275\",\"111138071136429601111\",\"7e1c20fc1848fe70\",[4,[0,[8,\"gdegz4x7zhgc9qqir\",\"foo\",[21,\"bar123\"]]]]]]]";
+    final String mapValueChangedMsg_delete = "[0,1369308664813,[" +
+            "[17,\"13ed12879c5\",\"111138071136429601111\",\"7e1c20fc1848fe70\",[4,[0,[8,\"gdegz4x7zhgc9qqir\",\"foo\"]]]]" +
+            "]]";
+    final String mapValueChangedMsg_add = "[0,1369308664833,[" +
+            "[19,\"13ed12bf275\",\"111138071136429601111\",\"7e1c20fc1848fe70\",[4,[0,[8,\"gdegz4x7zhgc9qqir\",\"foo\",[21,\"bar123\"]]]]]" +
+            "]]";
 
     // List events
-    final String listValuesAddedMsg_single = "[0,1369309187394,[[9,\"13ed13150e2\",\"111138071136429601111\",\"5c99913e2f7818c4\",[4,[0,[5,\"gde99vx3khg7uko2g\",0,[0,[21,\"bar\"]]]]]]]]";
-    final String listValuesAddedMsg_multiple = "[0,1369312739277,[[22,\"13ed16ff832\",\"111138071136429605372\",\"d4ece0c58170eef\",[4,[0,[5,\"gde99vx3khg7uko2g\",2,[0,[21,\"fooz\"]]]]]],[23,\"13ed16ff832\",\"111138071136429605372\",\"d4ece0c58170eef\",[4,[0,[5,\"gde99vx3khg7uko2g\",3,[0,[21,\"fooz\"]]]]]],[24,\"13ed16ff832\",\"111138071136429605372\",\"d4ece0c58170eef\",[4,[0,[5,\"gde99vx3khg7uko2g\",4,[0,[21,\"fooz\"]]]]]]]]";
-    final String listValuesSetMsg = "[0,1369309187437,[[15,\"13ed13577d0\",\"111138071136429601111\",\"5c99913e2f7818c4\",[4,[0,[11,\"gde99vx3khg7uko2g\",0,[0,[21,\"bazbaz\"]]]]]]]]";
-    final String listValuesRemovedMsg = "[0,1369309187439,[[16,\"13ed1359b20\",\"111138071136429601111\",\"5c99913e2f7818c4\",[4,[0,[6,\"gde99vx3khg7uko2g\",0,1]]]]]]";
+    final String listValuesAddedMsg_single = "[0,1369309187394,[" +
+            "[9,\"13ed13150e2\",\"111138071136429601111\",\"5c99913e2f7818c4\",[4,[0,[5,\"gde99vx3khg7uko2g\",0,[0,[21,\"bar\"]]]]]]" +
+            "]]";
+    final String listValuesAddedMsg_multiple = "[0,1369312739277,[" +
+            "[22,\"13ed16ff832\",\"111138071136429605372\",\"d4ece0c58170eef\",[4,[0,[5,\"gde99vx3khg7uko2g\",2,[0,[21,\"fooz\"]]]]]]," +
+            "[23,\"13ed16ff832\",\"111138071136429605372\",\"d4ece0c58170eef\",[4,[0,[5,\"gde99vx3khg7uko2g\",3,[0,[21,\"fooz\"]]]]]]," +
+            "[24,\"13ed16ff832\",\"111138071136429605372\",\"d4ece0c58170eef\",[4,[0,[5,\"gde99vx3khg7uko2g\",4,[0,[21,\"fooz\"]]]]]]" +
+            "]]";
+
+    final String listValuesSetMsg = "[0,1369309187437,[" +
+            "[15,\"13ed13577d0\",\"111138071136429601111\",\"5c99913e2f7818c4\",[4,[0,[11,\"gde99vx3khg7uko2g\",0,[0,[21,\"bazbaz\"]]]]]]" +
+            "]]";
+    final String listValuesRemovedMsg = "[0,1369309187439,[" +
+            "[16,\"13ed1359b20\",\"111138071136429601111\",\"5c99913e2f7818c4\",[4,[0,[6,\"gde99vx3khg7uko2g\",0,1]]]]" +
+            "]]";
 
     private RealtimeMessageHandler handler;
     private JsonFactory jsonFactory;
