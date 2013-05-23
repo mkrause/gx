475b172b2e25a2ba2179689b9a68876504330f5a
Yorick Holkamp <github@nextpulse.nl>
2013-05-23 12:24:52 +0200

    Split up the receive method into a message parser and handler for easier testing

--- src/gx/realtime/RealtimeMessageHandler.java
+++ src/gx/realtime/RealtimeMessageHandler.java
@@ -6,17 +6,40 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import gx.browserchannel.message.DataMessage;
 import gx.browserchannel.message.MessageEvent;
 import gx.browserchannel.message.MessageHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class RealtimeMessageHandler implements MessageHandler {
 
+    private static Logger logger = LogManager.getLogger(MessageHandler.class);
+
     @Override
     public void receive(MessageEvent e) {
+        logger.debug("Received messageEvent {}", e);
+        logger.debug("with message {}", e.getMessage());
+
         // Return on invalid data
         if(!(e.getMessage() instanceof DataMessage))
             return;
 
         // Parse data as event
         DataMessage data = (DataMessage) e.getMessage();
+        Event event = parseDataMessage(data);
+
+        // Handle events
+        if(event instanceof CollaboratorJoinedEvent) {
+            Collaborator user = ((CollaboratorJoinedEvent)event).getCollaborator();
+            System.out.println("Collaborator joined event: " + user.getDisplayName());
+        }
+    }
+
+    /**
+     * Parses a given DataMessage into an Event object.
+     * @param data
+     * @return
+     */
+    protected Event parseDataMessage(DataMessage data)
+    {
         JsonParser parser = data.getContent().traverse();
         parser.setCodec(new ObjectMapper());
         Event event = null;
@@ -25,11 +48,7 @@ public class RealtimeMessageHandler implements MessageHandler {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
-        // Handle events
-        if(event instanceof CollaboratorJoinedEvent) {
-            Collaborator user = ((CollaboratorJoinedEvent)event).getCollaborator();
-            System.out.println("Collaborator joined event: " + user.getDisplayName());
-        }
+        return event;
     }
+
 }
