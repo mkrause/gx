abd621215d671940b42c53add3344659995769b3
Roy de Bokx <info@debokxict.nl>
2013-05-30 12:56:36 +0200

    added javadoc to the CollaborativeObject

--- src/gx/realtime/CollaborativeObject.java
+++ src/gx/realtime/CollaborativeObject.java
@@ -16,7 +16,7 @@ public abstract class CollaborativeObject extends EventTarget{
 
 	//interfaces
 	public interface EventHandler{
-		public void execute(BaseModelEvent e);
+		public void handleEvent(BaseModelEvent e);
 	}
 	
 	//Attributes
@@ -25,17 +25,30 @@ public abstract class CollaborativeObject extends EventTarget{
 	private Map<EventType, Set<EventHandler>> eventHandlers;
 	
 	//Methods
-	
+
+    /**
+     * CollaborativeObject contains behavior common to all built in collaborative types. This class should not be instantiated directly.
+     * Use the create* methods on gx.realtime.Model to create specific types of collaborative objects.
+     * @param model The document model.
+     */
 	public CollaborativeObject(Model model){
 		this.model = model;
 		this.eventHandlers = new HashMap<EventType, Set<EventHandler>>();
 		//TODO: determine id of this object.
 	}
-	
+
+    /**
+     * Returns the object id.
+     * @return The id.
+     */
 	public String getId(){
 		return id;
 	}
-	
+
+    /**
+     * Returns a string representation of this collaborative object.
+     * @return A string representation.
+     */
 	public String toString(){
 		ObjectMapper mapper = new ObjectMapper();
 		StringWriter writer = new StringWriter();
@@ -47,7 +60,13 @@ public abstract class CollaborativeObject extends EventTarget{
 		
 		return writer.toString();
 	}
-	
+
+    /**
+     * Adds an event listener to the event target. The same handler can only be added once per the type.
+     * Even if you add the same handler multiple times using the same type then it will only be called once when the event is dispatched.
+     * @param type The type of the event to listen for.
+     * @param handler The function to handle the event. The handler can also be an object that implements the handleEvent method which takes the event object as argument.
+     */
 	public void addEventListener(EventType type, EventHandler handler){
     	Set<EventHandler> handlers = eventHandlers.get(type);
     	if(handlers == null){
@@ -57,20 +76,39 @@ public abstract class CollaborativeObject extends EventTarget{
     	handlers.add(handler);
     }
 
+    /**
+     * Removes an event listener from the event target. The handler must be the same object as the one added. If the handler has not been added then nothing is done.
+     * @param type The type of the event to listen for.
+     * @param handler The function to handle the event. The handler can also be an object that implements the handleEvent method which takes the event object as argument.
+     */
     public void removeEventListener(EventType type, EventHandler handler){
     	Set<EventHandler> handlers = eventHandlers.get(type);
     	if(handlers != null){
     		handlers.remove(handler);
     	}
     }
-    
-    protected void fireEvent(EventType type, BaseModelEvent event){
+
+    /**
+     * Dispatches the given event of the given event type to this object. According to the given EventType, the corresponding EventHandlers are executed.
+     * This method is only to be called by the gx.realtime.Document.
+     * @param type The event type.
+     * @param event The event object, containing any necessary information.
+     */
+    public void fireEvent(EventType type, BaseModelEvent event){
     	Set<EventHandler> handlers = eventHandlers.get(type);
     	for(EventHandler handler : handlers){
-    		handler.execute(event);
+    		handler.handleEvent(event);
     	}
     }
-    
+
+    /**
+     * This is an internal function for cloning objects of a given type. Examples are the CollaborativeMap and CollaborativeList which need to clone their contents
+     * when returning for instance an Array of the values.
+     * This is done by means of JSON serialization, in order to avoid constraining the used types to Cloneables.
+     * @param value The object that needs to be cloned.
+     * @param <E> The Object type.
+     * @return A clone of the given object.
+     */
 	protected <E> E clone(E value){
 		E clone = null;
 		ObjectMapper mapper = new ObjectMapper();
--- test/gx/realtime/CollaborativeObjectTest.java
+++ test/gx/realtime/CollaborativeObjectTest.java
@@ -6,33 +6,14 @@ import org.junit.Test;
 
 public class CollaborativeObjectTest {
 
-	@Test
-	public void testAddEventListener() {
-		fail("Not yet implemented");
-	}
+    
+    @Test
+    public void testCollaborativeObject() {
+        fail("Not yet implemented");
+    }
 
 	@Test
-	public void testRemoveEventListener() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCollaborativeObject() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetId() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCloneE() {
+	public void testEventListeners() {
 		fail("Not yet implemented");
 	}
 
