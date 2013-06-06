0fce02cba40d82286e9cfa3ad211a33198f3c89d
Yorick Holkamp <github@nextpulse.nl>
2013-06-06 11:24:49 +0200

    Added testcase for type 9 Operation JSON deserializing

---
+++ test/gx/realtime/serialize/OperationDeserializerTest.java
@@ -0,0 +1,43 @@
+package gx.realtime.serialize;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gx.realtime.operation.Operation;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.fail;
+
+/**
+ *
+ */
+public class OperationDeserializerTest
+{
+    final String emptyType9Operation = "[9]";
+
+    private JsonFactory jsonFactory = new JsonFactory();
+
+    @Test
+    public void testDeserialize_single() throws Exception
+    {
+        JsonParser parser = getParser(emptyType9Operation);
+
+        Operation event = parser.readValueAs(Operation.class);
+        assertNotNull("Empty operation of type 9 should be parsed correctly", event);
+    }
+
+    private JsonParser getParser(String json) {
+        JsonParser parser = null;
+        try {
+            parser = jsonFactory.createParser(json);
+            parser.setCodec(new ObjectMapper());
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Invalid JSON: ||START||" + json + "||END||");
+        }
+        return parser;
+    }
+}
