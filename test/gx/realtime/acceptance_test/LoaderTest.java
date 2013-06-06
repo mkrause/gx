50d1d2a4240397bd55b989c8bc41649e7c892e01
mkrause <maikelkrause@gmail.com>
2013-06-06 12:57:40 +0200

    Add verification using Mockito in the LoaderTest.

--- test/gx/realtime/acceptance_test/LoaderTest.java
+++ test/gx/realtime/acceptance_test/LoaderTest.java
@@ -5,6 +5,7 @@ import gx.realtime.IdentificationLoader;
 import gx.realtime.RealtimeLoader;
 import gx.realtime.RealtimeOptions;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.io.IOException;
 
@@ -16,8 +17,11 @@ import static org.mockito.Mockito.*;
  */
 public class LoaderTest
 {
-    @Test
-    public void testLoader()
+    /**
+     * Get the client ID from the default configuration file.
+     * @return
+     */
+    private String getClientId()
     {
         String clientId;
         try {
@@ -25,22 +29,27 @@ public class LoaderTest
         } catch (IOException e) {
             fail("Could not load client ID");
             e.printStackTrace();
-            return;
+            System.exit(1);
+            return null;
         }
-        
+        return clientId;
+    }
+    
+    @Test
+    public void testDocumentIsLoaded()
+    {
         RealtimeOptions options = new RealtimeOptions();
-        options.setClientId(clientId);
+        options.setClientId(getClientId());
         options.setDocId("0B6gFLa0mYRcOc3NKTmtwdV9idHM");
-
-        options.setInitializeModel((model) -> {
-            System.out.println("we haz model");
-        });
         
         options.setOnFileLoaded((document) -> {
             System.out.println("we haz document");
         });
         
         RealtimeLoader loader = new RealtimeLoader(options);
+        
         loader.start();
+        
+        verify(options.getOnFileLoaded()).onFileLoaded(isA(Document.class));
     }
 }
