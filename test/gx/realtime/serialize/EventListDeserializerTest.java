79b0d5c2c6676c2d0c97986c1c7c7d84100ae569
Erik van der Veen <erikvdv1@gmail.com>
2013-06-13 12:43:53 +0200

    More EventList tests

--- test/gx/realtime/serialize/EventListDeserializerTest.java
+++ test/gx/realtime/serialize/EventListDeserializerTest.java
@@ -1,10 +1,13 @@
 package gx.realtime.serialize;
 
 import com.fasterxml.jackson.core.JsonParser;
+import gx.realtime.CollaboratorJoinedEvent;
+import gx.realtime.CollaboratorLeftEvent;
 import gx.realtime.EventList;
+import gx.realtime.ObjectChangedEvent;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.*;
 import static junit.framework.TestCase.assertNotNull;
 
 /**
@@ -12,23 +15,71 @@ import static junit.framework.TestCase.assertNotNull;
  */
 public class EventListDeserializerTest extends DeserializerTestHelper
 {
-    private String type0Json = "[0,1370512608357,[[85,\"13f18ead3fd\",\"111138071136429605372\",\"4cd149cb40105a1f\",[9]]]]";
-    private String type1Json = "[1,1370512608357,{\"CONTENT_MODIFICATION_DATE\":true}]";
+    private String objectChangedEvent = "[85,\"13f18ead3fd\",\"111138071136429605372\",\"4cd149cb40105a1f\",[9]]";
+    private String objectChangedEmpty = "[0,1370512608357,[]]";
+    private String objectChangedSingle = "[0,1370512608357,["+objectChangedEvent+"]]";
+    private String objectChangedMultiple = "[0,1370512608357,["+objectChangedEvent+","+objectChangedEvent+","+objectChangedEvent+"]]";
+    private String contentModification = "[1,1370512608357,{\"CONTENT_MODIFICATION_DATE\":true}]";
+    private String user = "{\"color\":\"#000000\",\"displayName\":\"Test User\",\"isActive\":true,\"isAnonymous\":false,\"isMe\":true,\"isNew\":true,\"photoUrl\":\"\\/\\/domain.com\\/photo.jpg\",\"profileId\":\"112956879592017678596\",\"sid\":\"2221a682069e7485\",\"userId\":\"112656379192087458580\",\"userType\":0}";
+    private String userJoined = "[5,1370512608357,"+user+"]";
+    private String userLeft = "[6,1370512608357,{\"sid\":\"31d9317c707d16a1\",\"userId\":\"112956879592017678596\"}]";
 
     @Test
-    public void testDeserialize_type0() throws Exception
+    public void testDeserialize_objectChanged_empty() throws Exception
     {
-        JsonParser parser = getParser(type0Json);
+        JsonParser parser = getParser(objectChangedEmpty);
+
+        EventList eventList = parser.readValueAs(EventList.class);
+        assertNotNull(eventList);
+        assertEquals(0, eventList.getEvents().size());
+    }
+    @Test
+    public void testDeserialize_objectChanged_single() throws Exception
+    {
+        JsonParser parser = getParser(objectChangedSingle);
 
         EventList eventList = parser.readValueAs(EventList.class);
         assertNotNull(eventList);
         assertEquals(1, eventList.getEvents().size());
+        assertTrue("Event should be of type ObjectChangedEvent", eventList.getEvents().get(0) instanceof ObjectChangedEvent);
     }
 
     @Test
-    public void testDeserialize_type1() throws Exception
+    public void testDeserialize_objectChanged_multiple() throws Exception
     {
-        JsonParser parser = getParser(type1Json);
+        JsonParser parser = getParser(objectChangedMultiple);
+
+        EventList eventList = parser.readValueAs(EventList.class);
+        assertNotNull(eventList);
+        assertEquals(3, eventList.getEvents().size());
+    }
+
+    @Test
+    public void testDeserialize_userJoined() throws Exception
+    {
+        JsonParser parser = getParser(userJoined);
+
+        EventList eventList = parser.readValueAs(EventList.class);
+        assertNotNull(eventList);
+        assertEquals(1, eventList.getEvents().size());
+        assertTrue("Event should be of type CollaboratorJoinedEvent", eventList.getEvents().get(0) instanceof CollaboratorJoinedEvent);
+    }
+
+    @Test
+    public void testDeserialize_userLeft() throws Exception
+    {
+        JsonParser parser = getParser(userLeft);
+
+        EventList eventList = parser.readValueAs(EventList.class);
+        assertNotNull(eventList);
+        assertEquals(1, eventList.getEvents().size());
+        assertTrue("Event should be of type CollaboratorLeftEvent", eventList.getEvents().get(0) instanceof CollaboratorLeftEvent);
+    }
+
+    @Test
+    public void testDeserialize_contentModification() throws Exception
+    {
+        JsonParser parser = getParser(contentModification);
 
         EventList eventList = parser.readValueAs(EventList.class);
         assertNotNull(eventList);
