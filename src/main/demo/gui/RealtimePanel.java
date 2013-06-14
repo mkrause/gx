8427e0ddf17da09a497e76ae3597d350f3e44b88
Erik van der Veen <erikvdv1@gmail.com>
2013-06-14 20:53:49 +0200

    Graceful disconnect. Fixed #34

--- src/main/demo/gui/RealtimePanel.java
+++ src/main/demo/gui/RealtimePanel.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 /**
@@ -55,9 +56,10 @@ public class RealtimePanel extends JPanel
 
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.addWindowStateListener((WindowEvent e) -> {
-            System.out.println("Received WindowEvent " + e + ", should be politely closing now");
-            if (e.getNewState() == WindowEvent.WINDOW_CLOSED) {
+        frame.addWindowListener(new WindowAdapter()
+        {
+            public void windowClosing(WindowEvent e)
+            {
                 System.out.println("Politely closing API link...");
                 document.close();
             }
